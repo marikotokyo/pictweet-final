c6650f0968924b31625f4a4a781c042bf1d03f0d
marikotokyo <marikokanefuji@MarikonoMacBook-ea.local>
2018-12-20 18:37:25 +0900

    “users_info”

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,23 +3,37 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id # 後置ifで記述
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id]) #既に存在しているレコードを選択して中身を置き換えたいので、編集したいレコードを@tweetに代入して編集画面で利用できるようにする
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    #ビューから受け取ったパラメータからidを受け取り、それを元にtweetsテーブルからレコードを取得
+    tweet.update(tweet_params) if tweet.user_id ==current_user.id
   end
 
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
   end
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_053229) do
+ActiveRecord::Schema.define(version: 2018_12_20_072828) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.text "text"
     t.text "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
