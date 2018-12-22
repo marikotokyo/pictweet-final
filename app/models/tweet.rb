eb6733407d7dfd60e7e674620d1aa3b951a69117
marikotokyo <marikokanefuji@MarikonoMacBook-ea.local>
2018-12-22 12:58:57 +0900

    “controller_test”

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord  # $ rails g model tweetコマンドで作成
-  belongs_to :user #usersテーブルとのアソシエーション
+  belongs_to :user, optional: true #usersテーブルとのアソシエーション
+  #oputional:trueはコントローラのテストのため追記
   has_many :comments #commentsテーブルとのアソシエーション
 end
